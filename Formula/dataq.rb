110e753825180d3326e5523b53aadbc069c28e5f
straydog <horadog1991@gmail.com>
2026-06-08 14:56:01 +0900

    dataq v0.1.18

--- Formula/dataq.rb
+++ Formula/dataq.rb
@@ -2,7 +2,7 @@ class Dataq < Formula
   desc "Rust-native CLI for deterministic data preprocessing"
   homepage "https://github.com/koizumikento/dataq"
   license "MIT"
-  version "0.1.17"
+  version "0.1.18"
   depends_on "jq"
   depends_on "yq"
   depends_on "miller"
@@ -19,11 +19,11 @@ class Dataq < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/koizumikento/dataq/releases/download/v0.1.17/dataq-v0.1.17-aarch64-apple-darwin.tar.gz"
-      sha256 "3e39576725b151ea7e1479bcf608c3d0bc4fbd5d95ceca8ea7d31c7f011ccc1f"
+      url "https://github.com/koizumikento/dataq/releases/download/v0.1.18/dataq-v0.1.18-aarch64-apple-darwin.tar.gz"
+      sha256 "6eb3e15366a56d8396407680ed7c7855d36d18b9fc89f360e71edc8fc6343caa"
     else
-      url "https://github.com/koizumikento/dataq/releases/download/v0.1.17/dataq-v0.1.17-x86_64-apple-darwin.tar.gz"
-      sha256 "2c04f213138ae3cf38da843dda6fc29231d76d99fdb2cfa8ea772dd9f4511f0e"
+      url "https://github.com/koizumikento/dataq/releases/download/v0.1.18/dataq-v0.1.18-x86_64-apple-darwin.tar.gz"
+      sha256 "d22b01906d3d3a6e48eac2fb847475e75720edc7e3ad70e0fe225f6e2471ce13"
     end
   end
 
